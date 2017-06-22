43e745504856fe2b632b3c3824a1f003fcdad683
David-Rodden <davidrodden273@gmail.com>
2017-06-21 23:38:42 -0700

    Initial commit for Explorer

---
+++ desktop/src/com/rodden/explorer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rodden.explorer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rodden.explorer.ExplorerMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ExplorerMain(), config);
+	}
+}
